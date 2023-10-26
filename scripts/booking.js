dd98bfd06d29439ff3d2a08c9abe44c99b1cd8b7
alstdu <lexi.dugo@gmail.com>
2023-10-25 23:13:33 -0700

    add script to button

--- scripts/booking.js
+++ scripts/booking.js
@@ -1,10 +1,5 @@
-// todo: grab the /relevant/ form and put it here
 document.querySelector( '.pet-sitting-form' ).addEventListener( 'submit', ( e ) => {
     e.preventDefault(); // it's 2023 we don't reload the page on form submission
-    console.log( 'submit' );
 
-    // TODO: grab whatever element we have "hide" on and remove that class
     document.querySelector( '.section-3' ).className = 'section-3';
 } );
-
-console.log('foo');
